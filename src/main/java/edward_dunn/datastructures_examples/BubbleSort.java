4a8eb93c4fb223f45fb29897fc60d1a806f7914a
EdwardDunn <edwarddunn12@gmail.com>
2018-11-16 15:15:42 +0000

    added Bubble Sort comments and renamed variables for clarity

--- src/main/java/edward_dunn/datastructures_examples/BubbleSort.java
+++ src/main/java/edward_dunn/datastructures_examples/BubbleSort.java
@@ -2,34 +2,48 @@ package edward_dunn.datastructures_examples;
 
 public class BubbleSort {
 
-	static void bubbleSort(int [] arr) {
-		int n = arr.length;
-		int temp = 0;
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 1; j < (n-i); j++) {
-				if(arr[j-1] > arr[j]) {
-					temp = arr[j-1];
-					arr[j-1] = arr[j];
-					arr[j] = temp;
+	static void bubbleSort(int[] numArray) {
+		int n = numArray.length;
+		int tempValue = 0;
+
+		// loop n number of times (size of array)
+		for (int i = 0; i < n; i++) {
+
+			// loop over each of the elements left in the array to sort, at start n-i would
+			// be 0
+			for (int j = 1; j < (n - i); j++) {
+
+				// if the previous element is more than the current, swap
+				if (numArray[j - 1] > numArray[j]) {
+					tempValue = numArray[j - 1];
+					numArray[j - 1] = numArray[j];
+					numArray[j] = tempValue;
 				}
+
 			}
+
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		int arr[] = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
+		int numArray[] = { 1, 5, 1, -5, 9, 55, 77, -25, 100, 2000, 6 };
 		System.out.println("Array Before Bubble Sort");
-		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
+
+		//print current array structure
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.println(numArray[i] + " ");
 		}
-		System.out.println();
-		bubbleSort(arr);
-		System.out.println("Array After Bubble Sort");
 		
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
+		System.out.println();
+		
+		//sort array 
+		bubbleSort(numArray);
+		
+		System.out.println("Array After Bubble Sort");
+
+		//print sorted array elements
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.println(numArray[i] + " ");
 		}
 	}
 }
