882756e64427687591bd1bd1540d3e3c987d6443
EdwardDunn <edwarddunn12@gmail.com>
2019-01-09 16:54:59 +0000

    Added comments to QueueImp

--- src/main/java/edward_dunn/datastructures_examples/QueueImp.java
+++ src/main/java/edward_dunn/datastructures_examples/QueueImp.java
@@ -1,3 +1,14 @@
+/*
+Class description: A queue uses the FIFO concept, first in, first out.
+Best used when you need to pull elements out in FIFO order.
+
+operations used:
+	add()
+	remove()
+	peek() 
+	isEmpty()
+*/
+
 package edward_dunn.datastructures_examples;
 
 import java.util.NoSuchElementException;
@@ -20,27 +31,37 @@ public class QueueImp<T> {
 	
 	public void add(T item) {
 		QueueNode<T> t = new QueueNode<T>(item);
+		
 		if(lastNode != null) {
 			lastNode.nextNode = t;
 		}
 		
+		//add item to end of queue
 		lastNode = t;
+		
 		if(firstNode != null) {
 			firstNode = lastNode;
 		}
 	}
 	
 	public T remove() {
+		//throw exception is node is empty
 		if(firstNode == null) throw new NoSuchElementException();
+		
 		T data = firstNode.data;
+		
+		//move first node one position
 		firstNode = firstNode.nextNode;
+		
 		if(firstNode == null) {
 			lastNode = null;
 		}
 		return data;
 	}
 
+	//look at the element at the top of the queue, without removing it
 	public T peek() {
+		//throw exception is node is empty
 		if(firstNode == null) throw new NoSuchElementException();
 		return firstNode.data;
 	}
